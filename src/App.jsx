d00764bea93316c0663c857b53bec30c9dcac24e
programmer-kabir <programmerkabirr@gmail.com>
2023-08-03 12:48:02 +0600

    animation added

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ const App = () => {
   const [dropdownOpen, setDropdownOpen] = useState(null);
 
   return (
+    <>
     <div className="flex">
-      <Style name='Location' options={['Option 1', 'Option 2', 'Option 3']} open={dropdownOpen} setOpen={setDropdownOpen} />
+      <Style name='Location' options={['Option 1', 'Option 2', 'Option 3']} open={dropdownOpen} setOpen={setDropdownOpen} isFirst />
       <Style name='Property Size' options={['Option 1', 'Option 2', 'Option 3']} open={dropdownOpen} setOpen={setDropdownOpen} />
       <Style name='Price Range $' options={['Option 1', 'Option 2', 'Option 3']} open={dropdownOpen} setOpen={setDropdownOpen} />
     </div>
+    <button>Search</button>
+    </>
   );
 };
 
--- src/Style.jsx
+++ src/Style.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BiSolidDownArrow } from "react-icons/bi";
-const Style = ({ name, options, open, setOpen }) => {
+const Style = ({ name, options, open, setOpen, isFirst  }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
@@ -13,23 +13,80 @@ const Style = ({ name, options, open, setOpen }) => {
     setOpen(isOpen ? null : name);
     setIsOpen(!isOpen);
   };
-
+//   const divClass = isFirst ? "relative border-2 border-blue-800 rounded-l-md inline-block text-left" : "relative border-2 border-blue-800 inline-block text-left";
+const divClass = isFirst ? "relative border-2 border-blue-800 rounded-l-full inline-block text-left" : "relative border-r-2 border-t-2 border-b-2 border-blue-800 inline-block text-left";
   return (
+    // <div>
+    //   <div className={divClass}>
+    //     <button
+    //       type="button"
+    //       onClick={handleToggle}
+    //       className="inline-flex items-center gap-4 justify-center w-full rounded-md shadow-sm px-4 py-2  text-sm font-medium text-gray-700  "
+    //       id="options-menu"
+    //       aria-haspopup="true"
+    //       aria-expanded="true"
+    //     >
+    //       {name}
+    //       <BiSolidDownArrow className="-mr-1 ml-2 h-4 w-4 " color="blue" />
+    //     </button>
+    //     {isOpen && (
+    //       <div className="origin-top-right absolute mt-2 w-56 rounded-md shadow-2xl bg-white ring-1 ring-black ring-opacity-5">
+    //         <div
+    //           className="py-1"
+    //           role="menu"
+    //           aria-orientation="vertical"
+    //           aria-labelledby="options-menu"
+    //         >
+    //           {options.map((option, index) => (
+    //             <a
+    //               key={index}
+    //               href="#"
+    //               className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+    //               role="menuitem"
+    //             >
+    //               {option}
+    //             </a>
+    //           ))}
+    //         </div>
+    //       </div>
+    //     )}
+    //   </div>
+    // </div>
     <div>
-      <div className="relative border border-gray-700 inline-block text-left">
+      <div className={divClass}>
         <button
           type="button"
           onClick={handleToggle}
-          className="inline-flex items-center gap-4 justify-center w-full rounded-md shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+          className="inline-flex items-center gap-4 justify-center w-full rounded-md shadow-sm px-4 py-2  text-sm font-medium text-gray-700  "
           id="options-menu"
           aria-haspopup="true"
           aria-expanded="true"
         >
           {name}
-          <BiSolidDownArrow className="-mr-1 ml-2 h-4 w-4" />
+          <BiSolidDownArrow className="-mr-1 ml-2 h-4 w-4 " color="blue" />
         </button>
-        {isOpen && (
-          <div className="origin-top-right absolute mt-2 w-56 rounded-md shadow-2xl bg-white ring-1 ring-black ring-opacity-5">
+        {isOpen ? (
+          <div className="origin-top-right absolute mt-2 w-56 rounded-md shadow-2xl bg-white ring-1 ring-black ring-opacity-5 transition-all duration-500 overflow-hidden max-h-96">
+            <div
+              className="py-1"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="options-menu"
+            >
+              {options.map((option, index) => (
+                <a
+                  key={index}
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                  role="menuitem"
+                >
+                  {option}
+                </a>
+              ))}
+            </div>
+          </div>
+        ) : (
+          <div className="origin-top-right absolute mt-2 w-56 rounded-md shadow-2xl bg-white ring-1 ring-black ring-opacity-5 transition-all duration-500 overflow-hidden max-h-0">
             <div
               className="py-1"
               role="menu"
